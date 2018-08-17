27571dc6acf6cb6e995432c0e0170823b58b584c
Allan Kerr <personal.allankerr@gmail.com>
2018-08-17 00:39:32 -0400

    Move the register submission action from the form into the view

--- ClientApp/src/components/RegisterForm.js
+++ ClientApp/src/components/RegisterForm.js
@@ -3,27 +3,19 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FormInput, FormSubmitButton } from './forms/FormComponents';
 import { actionCreators } from '../store/Register';
-import XsrfProtection from './security/XsrfProtection';
 import { bindActionCreators } from 'redux';
 
 class RegisterForm extends Component {
-  register = event => {
+  onSubmit = event => {
     event.preventDefault();
-
-    const target = event.target;
-    const data = {
-      email: target.Email.value,
-      password: target.Password.value,
-      confirmPassword: target.ConfirmPassword.value
-    };
-    this.props.register(this.props.xsrfToken, data);
+    this.props.onSubmit(event);
   };
 
   render() {
     return (
       <div>
         <h1>Create an account.</h1>
-        <form type="submit" onSubmit={this.register}>
+        <form type="submit" onSubmit={this.onSubmit}>
           <FormInput
             type="email"
             name="Email"
@@ -56,4 +48,4 @@ class RegisterForm extends Component {
 export default connect(
   state => state.register,
   dispatch => bindActionCreators(actionCreators, dispatch)
-)(XsrfProtection(RegisterForm));
+)(RegisterForm);
--- ClientApp/src/components/RegisterView.js
+++ ClientApp/src/components/RegisterView.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import RegisterForm from './RegisterForm';
 import FormCard from './FormCard';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import XsrfProtection from './security/XsrfProtection';
+import { actionCreators } from '../store/Register';
+import { Redirect } from 'react-router-dom';
 
 class RegisterView extends Component {
+  onSubmit = event => {
+    const target = event.target;
+    const data = {
+      email: target.Email.value,
+      password: target.Password.value,
+      confirmPassword: target.ConfirmPassword.value
+    };
+    this.props.register(this.props.xsrfToken, data);
+  };
+
   render() {
+    if (this.props.user != null) {
+      return <Redirect to="/home" />;
+    }
     return (
       <FormCard>
-        <RegisterForm />
+        <RegisterForm onSubmit={this.onSubmit} />
       </FormCard>
     );
   }
 }
 
-export default RegisterView;
+export default connect(
+  state => state.account,
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(XsrfProtection(RegisterView));
