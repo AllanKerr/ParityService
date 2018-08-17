8cbd4613b7bee2949d2e6551075506e9443d729f
Allan Kerr <personal.allankerr@gmail.com>
2018-08-17 00:07:04 -0400

    Remove login logic from Home

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,35 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import LoginFrom from './LoginForm';
-import FormCard from './FormCard';
+import React from 'react';
 
-class Home extends Component {
-  logIn(event) {
-    event.preventDefault();
+const Home = () => <h1>Home</h1>;
 
-    const data = {
-      email: event.target.email.value,
-      password: event.target.password.value,
-      rememberMe: event.target.rememberMe.checked
-    };
-    axios
-      .post('account/login', data)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <FormCard>
-        <LoginFrom />
-      </FormCard>
-    );
-  }
-}
-
-export default connect()(Home);
+export default Home;
