dcdc66cfa36647ddf9f5f5eae7e946391508c957
Allan Kerr <personal.allankerr@gmail.com>
2018-08-16 23:45:30 -0400

    Add nav bar with login, sign up, and log out buttons

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,4 +1,50 @@
-﻿import React from 'react';
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Logout';
+import XsrfProtection from './security/XsrfProtection';
+import { bindActionCreators } from 'redux';
+
 import './NavMenu.css';
 
-export default props => <div />;
+class NavMenu extends Component {
+  logout = event => {
+    this.props.logout(this.props.xsrfToken);
+  };
+
+  render() {
+    var userAction;
+    if (this.props.user == null) {
+      userAction = (
+        <div className="user-action-container">
+          <Link to="/" className="button primary medium">
+            Log In
+          </Link>
+          <Link to="/register" className="button secondary medium">
+            Sign Up
+          </Link>
+        </div>
+      );
+    } else {
+      userAction = (
+        <button onClick={this.logout} className="button secondary medium">
+          Log Out
+        </button>
+      );
+    }
+
+    return (
+      <header className="nav">
+        <Link className="logo" to="/">
+          Parity
+        </Link>
+        {userAction}
+      </header>
+    );
+  }
+}
+
+export default connect(
+  state => ({ ...state.account, ...state.logout }),
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(XsrfProtection(NavMenu));
