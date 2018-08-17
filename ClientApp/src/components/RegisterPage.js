07c0fffa73020123685ab1ab0375d8f4fe91b0b8
Allan Kerr <personal.allankerr@gmail.com>
2018-08-16 23:49:14 -0400

    Remove dead code from register page

--- ClientApp/src/components/RegisterPage.js
+++ ClientApp/src/components/RegisterPage.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
 import RegisterForm from './RegisterForm';
 import FormCard from './FormCard';
 
 class RegisterPage extends Component {
-  register(event) {
-    event.preventDefault();
-
-    const data = {
-      email: event.target.email.value,
-      password: event.target.password.value,
-      rememberMe: event.target.rememberMe.checked
-    };
-    axios
-      .post('account/login', data)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
   render() {
     return (
       <FormCard>
@@ -32,4 +12,4 @@ class RegisterPage extends Component {
   }
 }
 
-export default connect()(RegisterPage);
+export default RegisterPage;
