c85591372e4eea8092b6700aee5f1b5b628233db
Allan Kerr <personal.allankerr@gmail.com>
2018-08-16 23:43:46 -0400

    Remove unused import

--- ClientApp/src/components/RegisterForm.js
+++ ClientApp/src/components/RegisterForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FormInput, FormSubmitButton } from './forms/FormComponents';
 import { actionCreators } from '../store/Register';
