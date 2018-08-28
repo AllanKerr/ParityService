fd2d6853f343c5c4d71f06f4cebde4a77182e90c
Allan Kerr <personal.allankerr@gmail.com>
2018-08-28 00:11:07 -0400

    Add lazy loading for credentials

--- Models/AccountLink.cs
+++ Models/AccountLink.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Questrade = ParityService.Questrade.Models;
 
 namespace ParityUI.Models
 {
   public sealed class AccountLink
   {
+    private readonly ILazyLoader m_lazyLoader;
+
+    private Credentials m_credentials;
+
     public string Id { get; private set; }
 
     public string AppUserId { get; private set; }
@@ -15,10 +20,19 @@ namespace ParityUI.Models
 
     public DateTime CreatedAt { get; private set; }
 
-    public Credentials Credentials { get; private set; }
+    public Credentials Credentials
+    {
+      get => m_lazyLoader?.Load(this, ref m_credentials);
+      private set { m_credentials = value; }
+    }
 
-    private AccountLink() {}
-    
+    private AccountLink() { }
+
+    private AccountLink(ILazyLoader lazyLoader)
+    {
+      m_lazyLoader = lazyLoader;
+    }
+
     public AccountLink(string userId, bool isPractice)
     {
       AppUserId = userId;
