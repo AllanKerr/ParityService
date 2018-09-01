2d816196057dc68125591ae8dbe5130766ecf666
Allan Kerr <personal.allankerr@gmail.com>
2018-09-01 18:12:15 -0400

    Contribution limit migration

---
+++ Migrations/20180901220653_ContributionRoom.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParityService.Migrations
+{
+    public partial class ContributionRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ContributionRoom",
+                table: "Accounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContributionRoom",
+                table: "Accounts");
+        }
+    }
+}
