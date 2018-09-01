045ec48942d0ff60b20c787fc1b506c7e1d01628
Allan Kerr <personal.allankerr@gmail.com>
2018-09-01 16:12:30 -0400

    Add account ID and whether the account is managed to the view model

--- Models/View/AccountViewModel.cs
+++ Models/View/AccountViewModel.cs
@@ -7,16 +7,22 @@ namespace ParityService.Models.View
 {
   public sealed class AccountViewModel
   {
+    public int Id { get; }
+
     [Required]
     public string AccountName { get; set; }
 
     [Required]
     public AccountType AccountType { get; set; }
 
+    public bool IsManaged { get; private set; }
+
     public AccountViewModel(Account account)
     {
+      Id = account.Id;
       AccountName = account.AccountName;
       AccountType = account.AccountType;
+      IsManaged = account.ServiceLinkUserId != null;
     }
 
     public AccountViewModel() { }
